85c6c1cfe571356382c7dfd26987416054896297
Riya Sharma <riri.sharma05@gmail.com>
2025-10-23 16:07:28 +0100

    Set up the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+//  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+//class MyApp extends StatelessWidget {
+//  const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sandwich Shop',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Sandwich shop'),
-    );
-  }
-}
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Sandwich Shop',
+//      theme: ThemeData(
+//        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//        useMaterial3: true,
+//      ),
+//      home: const MyHomePage(title: 'Sandwich shop'),
+//    );
+//  }
+//}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
